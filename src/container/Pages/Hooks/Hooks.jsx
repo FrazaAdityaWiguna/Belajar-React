abbedc98961d1c40d14091b72b2f9636c6267f7a
Fraza Aditya Wiguna <67851409+FrazaAdityaWiguna@users.noreply.github.com>
2021-04-05 07:58:02 +0700

    Update folder

--- src/container/Pages/Hooks/Hooks.jsx
+++ src/container/Pages/Hooks/Hooks.jsx
@@ -1,25 +1,15 @@
-import React, { Component } from "react";
+import React, { useState } from "react";
 import "./Hooks.css";
 
-class Hooks extends Component {
-  state = {
-    count: 0,
-  };
+const Hooks = () => {
+  const [count, setCount] = useState(0);
 
-  onUpdate = () => {
-    this.setState({
-      count: this.state.count + 1,
-    });
-  };
-
-  render() {
-    return (
-      <div className="container-hooks">
-        <p>Nilai Sekarang: {this.state.count}</p>
-        <button onClick={this.onUpdate}>Update Value</button>
-      </div>
-    );
-  }
-}
+  return (
+    <div className="container-hooks">
+      <p>Nilai Sekarang: {count}</p>
+      <button onClick={() => setCount(count + 1)}>Update Value</button>
+    </div>
+  );
+};
 
 export default Hooks;
