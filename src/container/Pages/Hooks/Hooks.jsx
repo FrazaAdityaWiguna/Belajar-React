26d6021a2816e829e49b1f31b001058f0b15bef8
Fraza Aditya Wiguna <67851409+FrazaAdityaWiguna@users.noreply.github.com>
2021-04-05 20:29:57 +0700

    Materi Hooks
    Add materi Hooks useState dan useEffect

--- src/container/Pages/Hooks/Hooks.jsx
+++ src/container/Pages/Hooks/Hooks.jsx
@@ -1,8 +1,48 @@
-import React, { useState } from "react";
+import React, { Component, useState, useEffect } from "react";
 import "./Hooks.css";
+
+// class Hooks extends Component {
+//   state = {
+//     count: 0,
+//   };
+
+//   onUpdate = () => {
+//     this.setState({
+//       count: this.state.count + 1,
+//     });
+//   };
+
+//   componentDidMount() {
+//     document.title = `Title Change: ${this.state.count}`;
+//   }
+
+//   componentDidUpdate() {
+//     document.title = `Title Change: ${this.state.count}`;
+//   }
+
+//   componentWillUnmount() {
+//     document.title = "React JS";
+//   }
+
+//   render() {
+//     return (
+//       <div className="container-hooks">
+//         <p>Nilai Sekarang: {this.state.count}</p>
+//         <button onClick={this.onUpdate}>Update Value</button>
+//       </div>
+//     );
+//   }
+// }
 
 const Hooks = () => {
   const [count, setCount] = useState(0);
+
+  useEffect(() => {
+    document.title = `Title Change: ${count}`;
+    return () => {
+      document.title = "React JS";
+    };
+  });
 
   return (
     <div className="container-hooks">
@@ -13,3 +53,5 @@ const Hooks = () => {
 };
 
 export default Hooks;
+
+// Link Tutoral: https://www.youtube.com/watch?v=Iv-V_BTwuSA&list=PLU4DS8KR-LJ03qEsHn9zV4qdhcWtusBqb&index=35
